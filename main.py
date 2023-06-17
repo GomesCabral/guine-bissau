6dc04fc97616a670b1ce3f986088b1ece29454de
gomes cabral <phgcabral@hotmail.com>
2023-06-17 19:35:15 +0200

    all project

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 my_screen = turtle.Screen()
 my_screen.title('U.S states Game')
@@ -8,11 +9,32 @@ my_screen.addshape(image)
 
 turtle.shape(image)
 
+#HOW TO KNOW THE COORDENATES BY CLICKING THE MAP
 def get_mouse_click_coor(x, y):
     print(x, y)
-
 turtle.onscreenclick(get_mouse_click_coor)
 
 
-#KEEP THE SCREEN OPEN
-turtle.mainloop()
+
+#READ THE CSV
+data = pandas.read_csv("city.csv")
+cities = data.city.to_list()
+
+guessed_cities = []
+
+while len(cities) > 0:
+    user_answer = my_screen.textinput(title=f"{len(guessed_cities)}/{len(cities)}", prompt="Guess the City? ").title()
+
+    if user_answer == 'exit':
+        break
+
+    if user_answer in cities:
+        guessed_cities.append(user_answer)
+        heni = turtle.Turtle()
+        heni.hideturtle()
+        heni.penup()
+        all_data = data[data.city == user_answer]
+        heni.goto(int(all_data.x), int(all_data.y))
+        heni.color('red')
+        heni.write(all_data.city.item())
+        cities.remove(all_data.city.item())
